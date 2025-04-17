e305a4dcabb4b3d39d9ed33fa03a60b61e670873
Assile Zeidan <assile.zeidan@hotmail.com>
2025-04-17 12:08:27 +0200

    min height

--- frontend2/vite-project/src/App.jsx
+++ frontend2/vite-project/src/App.jsx
@@ -17,13 +17,18 @@ const App = () => {
     <>
       <Router>
         <NavBar/>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LogInPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/create-product" element={<CreateProduct />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <main
+          style={{ minHeight: "calc(100vh - 180px)" }}
+          className="container d-flex flex-column justify-content-center align-items-center"
+        >
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LogInPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/create-product" element={<CreateProduct />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
         <FooterComp/>
       </Router>
     </>
