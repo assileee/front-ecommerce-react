13ef6fd854de60864aea0a71196b26b8490bd22b
Assile Zeidan <assile.zeidan@hotmail.com>
2025-04-22 11:11:50 +0200

    errors and loading

--- frontend2/vite-project/src/pages/HomePage.jsx
+++ frontend2/vite-project/src/pages/HomePage.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { fetchProducts } from "../utils/fetchProducts.js"
 
 const HomePage = () => {
-    return (
-        <div>
-            <h1 className="text-center">App</h1>
-        </div>
-    )
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      try {
+        const data = await fetchProducts()
+        setProducts(data)
+        setError(null)
+      } catch (err) {
+        setError(err.message || "Something went wrong")
+      } finally {
+        setLoading(false)
+      }
+    }
+    loadProducts()
+  }, [])
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error}</p>
+  if (!products.length) return <p>No products found</p>
+  console.log(products)
+
+  return (
+    <div>
+      <h1 className="text-center">App</h1>
+      <ul>
+        {products.map((product,index) => (
+          <li key={index}>{product.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default HomePage
