7e7e099aee7f71b3ca84df5d3c449d24ae5f1972
Assile Zeidan <assile.zeidan@hotmail.com>
2025-04-26 16:45:36 +0200

    updating backend

--- src/models/productModels.js
+++ src/models/productModels.js
@@ -1,45 +1,11 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-const productSchema= new mongoose.Schema({
-    productName: {
-        type: String,
-        required: true,
-    },
 
-    productDescription: {
-        type: String,
-        required: true,
-    },
-    
-    imageUrl: {
-        type: Buffer,
-        required: true
-    },
+const productSchema = new mongoose.Schema({
+    id: Number,
+    name: String,
+    price: Number,
+    description: String,
+    imageUrl: String
+}, { collection: 'products' });
 
-    brand: {
-        type: String,
-        required: true
-    },
-
-    model: {
-        type: String,
-        required: true
-    },
-
-    stock: {
-        type: Number,
-        required: true
-    },
-
-    price: {
-        type: Number,
-        required: true
-    },
-},
-
-{timestamps: true}
-)
-
-productSchema.plugin(uniqueValidator)
-
-module.exports = mongoose.model("productSchema", productSchema)
+module.exports = mongoose.model("Product", productSchema, "products");
